92b3818e77c03d7ff804477d1313d3888b02cffc
acolistro <arcolistro@gmail.com>
2020-05-12 19:26:11 -0700

    add user output component with initial content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserInput from './User/UserInput';
+import UserOutput from './User/UserOutput';
 
 class App extends Component {
+
+
+
   render() {
     return (
       <div className="App">
