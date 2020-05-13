a52ae6ea1fe4f4da9a85da2bda84125a5fbdb5fc
acolistro <arcolistro@gmail.com>
2020-05-12 19:51:12 -0700

    add username prop to user output and pass it through to display component using props

--- src/User/UserOutput.js
+++ src/User/UserOutput.js
@@ -3,6 +3,7 @@ import React from 'react';
 const userOutput = (props) => {
   return (
     <div className="UserOutput">
+      <h3>{props.userName}</h3>
       <p>{props.paragraph1}</p>
       <p>{props.paragraph2}</p>
     </div>
