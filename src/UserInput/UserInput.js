924e15af1531043d31be4dac1acd086cdc092a03
acolistro <arcolistro@gmail.com>
2020-05-14 14:17:43 -0700

    begin refactor of app.js

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const userInput = (props) => {
   return (
-    <div className="UserInput">
       <input type="text" onChange={props.change} value={props.userName}/>
-    </div>
   )
 };
 
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import UserInput from './User/UserInput';
+
 
 const userOutput = (props) => {
   return (
     <div className="UserOutput">
-      <h3>{props.userName}</h3>
+      {/* <h3>{props.userName}</h3>
       <p>{props.paragraph1}</p>
-      <p>{props.paragraph2}</p>
+      <p>{props.paragraph2}</p> */}
+      <p>First Paragraph</p>
+      <p>Second Paragraph</p>
     </div>
   )
 };
