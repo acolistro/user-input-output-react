c84cfaad1bcb35ab3e425f2e9c065718d28f3121
acolistro <arcolistro@gmail.com>
2020-05-14 15:02:55 -0700

    add some styling

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const userInput = (props) => {
+  const inputStyle = {
+    border:'2px solid gold'
+  };
   return (
       <input type="text" 
+      style={inputStyle}
       onChange={props.changed} 
       value={props.currentName}/>
   )
